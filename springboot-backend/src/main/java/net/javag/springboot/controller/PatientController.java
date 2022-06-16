4db3af9e88d25629e363cde6111b55af78e17449
ShriramGadewar <gadewarshriram@gmail.com>
2022-06-16 09:45:21 +0530

    First Commit

--- springboot-backend/src/main/java/net/javag/springboot/controller/PatientController.java
+++ springboot-backend/src/main/java/net/javag/springboot/controller/PatientController.java
@@ -2,12 +2,16 @@ package net.javag.springboot.controller;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import net.javag.springboot.exception.ResourceNotFoundException;
 import net.javag.springboot.model.patient;
 import net.javag.springboot.repository.PatientRepository;
 
@@ -28,4 +32,12 @@ public class PatientController {
 	public patient createPatient(@RequestBody patient Patient) {
 		return patientRepository.save(Patient);
 	}
+	
+	@GetMapping("/patient/{id}")
+	public ResponseEntity<patient> getPatientById(@PathVariable Long id){
+		patient Patient1 = patientRepository.findById(id).orElseThrow(() -> 
+		new ResourceNotFoundException("patient not exist of id =" + id));
+		
+		return ResponseEntity.ok(Patient1);
+	}	
 }
